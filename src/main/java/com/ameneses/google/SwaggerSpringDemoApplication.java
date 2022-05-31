e7d8320a1bd6c4a0cffc3d7529ae09e6663b75da
AldoMeneses86 <89155654+AldoMeneses86@users.noreply.github.com>
2022-05-31 18:50:46 -0500

    new features
    new features

--- src/main/java/com/ameneses/google/SwaggerSpringDemoApplication.java
+++ src/main/java/com/ameneses/google/SwaggerSpringDemoApplication.java
@@ -2,9 +2,16 @@ package com.ameneses.google;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class SwaggerSpringDemoApplication {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String healthCheck() {
+		return "200 OK";
+	}
     public static void main(String[] args) {
 		SpringApplication.run(SwaggerSpringDemoApplication.class, args);
 	}
